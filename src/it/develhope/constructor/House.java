53bede5080be41ac493c41aca8ad8f80ba6ce8a2
iosbd <io232001@gmail.com>
2023-01-24 15:41:03 +0100

    Initial commit

---
+++ src/it/develhope/constructor/House.java
@@ -0,0 +1,11 @@
+package it.develhope.constructor;
+
+public class House
+{
+    public String adress;
+    public int numbersOfFloors;
+    public House (String adress,int numbersOfFloors)
+    {
+        System.out.println("the house is located in "+adress+" that has "+numbersOfFloors+" floors");
+    }
+}
---
+++ src/it/develhope/constructor/Tester.java
@@ -0,0 +1,11 @@
+package it.develhope.constructor;
+
+public class Tester
+{
+    public static void main(String ...args)
+    {
+        House house1, house2;
+        house1 = new House("Via roma 2",3);
+        house2 = new House("Via Torino 31", 4);
+    }
+}
